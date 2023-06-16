ee7e70353f6708d8ce4635e108451748cd26259f
felix <felixnduhiu@gmail.com>
2023-06-16 09:28:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Kor
-par, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Kor par, 2015-10-19\n", stdout);
 Return(1);
 }
