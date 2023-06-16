5a6038c8b934afa8094ab3ecc1aaef445edf0dfd
felix <felixnduhiu@gmail.com>
2023-06-16 09:39:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 /**
  * main - Program that prints line to the starndard error
  *
- * Return: 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2,
-			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	Return (1);
 }
