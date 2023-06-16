3ea5d9292c1ce23fdf57c45d849878ae11e36e45
felix <felixnduhiu@gmail.com>
2023-06-16 09:05:03 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Program that prints line to the starndard error
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Kor
+par, 2015-10-19\r", stdout);
+Return(1);
+}
