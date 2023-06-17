baf8fabc993fad98f2d6556f86748ff29251fce8
felix <felixnduhiu@gmail.com>
2023-06-17 08:57:32 -0700

    /3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char d;
+
+	char c;
+
+	d = 'a';
+	c = 'A';
+	while
+		(d <= 'z') {
+			putchar (d);
+			d++;
+		}
+	while
+		(c <= 'Z') {
+			putchar (c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
