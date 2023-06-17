629e7da886a4ca4af60959d2f4bac51c82b4e3b7
felix <felixnduhiu@gmail.com>
2023-06-17 10:18:18 -0700

    4-print_alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char alp[24] =
+	"abcdfghijklmnoprstuvwxyz";
+
+	int r;
+
+	for (r = 0; r <= 24; r++)
+	{
+		putchar(alp[r]);
+	}
+	putchar('\n');
+
+	return (0);
+}
