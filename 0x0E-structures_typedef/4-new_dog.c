8130ec5941177770608856248ad016263e0bec0c
felix <felixnduhiu@gmail.com>
2023-07-18 13:11:30 -0700

    A door is what a dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,10 +60,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
+
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+	{
+		free(dog);
 		return (NULL);
-	dog->owner = malloc(sizeof(char) * (len1 + 1));
+
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
